4d030c1bd9ef19a1fc22379f91f0d4963c0ccf70
EnzoMGitHub <vincenzomonterosso9@gmail.com>
2026-03-31 10:36:41 -0400

    head of dash

--- server/cache.js
+++ server/cache.js
@@ -1,7 +1,4 @@
-/**
- * Temporary no-op cache adapter.
- * Keeps the same function signature so callers do not need to change.
- */
+
 export async function getCachedValue(_cacheKey, _ttlMs, loader) {
   if (typeof loader !== "function") {
     throw new TypeError("getCachedValue not implemented yet, come back later!!!");
